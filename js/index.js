d0b8df8aa757239277b61e420f3852a36142b277
Yanis Urbis <yanis.urbis@gmail.com>
2018-12-23 21:12:12 +0300

    align sliders

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-const openContactUsModal = () => {
-    console.log("opening modal");
-};
-
-const closeContactUsModal = () => {
-    console.log("closing modal");
-};
-
-const openMapModal = () => {
-    console.log("1");
-};
-
-const closeMapModal = () => {
-    console.log("1");
-};
-
 const contactUsBtn = document.getElementById("contact-us");
 const routeToUsBtn = document.querySelector("button.map");
 const contactFormModal = document.querySelector(".modal.contact-form");
@@ -24,3 +8,33 @@ contactUsBtn.addEventListener("click", () => contactFormModal.classList.toggle("
 routeToUsBtn.addEventListener("click", () => routeToUsMapModal.classList.toggle("hidden"))
 routeToUsMapModalCloseBtn.addEventListener("click", () => routeToUsMapModal.classList.toggle("hidden"));
 contactFormModalCloseBtn.addEventListener("click", () => contactFormModal.classList.toggle("hidden"));
+
+
+const updateClasses = (list, slideIndexToDisplay) => {
+  list.forEach((slide, slideIndex) => {
+    if (slideIndex === slideIndexToDisplay) {
+      slide.classList.add("current");
+    } else {
+      slide.classList.remove("current");
+    }
+  })
+}
+
+const addToggleLogic = (controllers, slides) => {
+  controllers.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      updateClasses(controllers, index);
+      updateClasses(slides, index);
+    });
+  })
+}
+
+const sliderControllers = document.querySelectorAll(".slider-controllers button");
+const slides = document.querySelectorAll(".featured-products .product");
+
+addToggleLogic(sliderControllers, slides);
+
+const servicesControllers = document.querySelectorAll(".services button");
+const servicesSlides = document.querySelectorAll(".services .service");
+
+addToggleLogic(servicesControllers, servicesSlides);
